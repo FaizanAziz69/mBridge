45d6c594b4e49630332b12116a2c15ad40859de5
FaizanAziz69 <faizanaziz69gmail.com>
2025-06-03 17:18:54 +0500

    change footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 "use client";
 
 export * from "./navbar";
-export * from "./footer";
+// export * from "./footer";
 export * from "./layout";
-export * from "./footer";
+// export * from "./footer";
 export * from "./info-card";
 
