22dc98ea650684ea5c8db1523a2ceeada91b13ee
FaizanAziz69 <faizanaziz69gmail.com>
2025-06-03 17:37:17 +0500

    update build

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,5 @@ export * from "./navbar";
 // export * from "./footer";
 export * from "./layout";
 // export * from "./footer";
-export * from "./info-card";
+// export * from "./info-card";
 
--- src/components/info-card.tsx
+++ src/components/info-card.tsx
@@ -1,24 +1,24 @@
-import React from "react";
+// import React from "react";
 
-import { Typography, Card, CardBody } from "@material-tailwind/react";
+// import { Typography, Card, CardBody } from "@material-tailwind/react";
 
-interface InfoCardProps {
-  title: string;
-  children: React.ReactNode;
-}
+// interface InfoCardProps {
+//   title: string;
+//   children: React.ReactNode;
+// }
 
 
-export function InfoCard({ title, children }: InfoCardProps) {
-  return (
-    <Card color="transparent" shadow={false} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-      <CardBody className="grid px-0"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-        <Typography variant="h2" color="blue-gray" className="mb-2" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-          {title}
-        </Typography>
-        <Typography className=" font-normal"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>{children}</Typography>
-      </CardBody>
-    </Card>
-  );
-}
+// export function InfoCard({ title, children }: InfoCardProps) {
+//   return (
+//     <Card color="transparent" shadow={false} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+//       <CardBody className="grid px-0"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+//         <Typography variant="h2" color="blue-gray" className="mb-2" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+//           {title}
+//         </Typography>
+//         <Typography className=" font-normal"  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>{children}</Typography>
+//       </CardBody>
+//     </Card>
+//   );
+// }
 
-export default InfoCard;
+// export default InfoCard;
